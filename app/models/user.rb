18949fd60e0ba3b43cd4d47cb05400ddc3f7f07e
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 02:49:57 -0400

    Chaned available devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are: :recoverable, :rememerable,
+  # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :validatable, :omniauthable
 end
