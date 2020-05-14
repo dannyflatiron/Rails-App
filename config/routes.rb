ece926176d6487e50edaa78d5487e4f910aa73d9
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 11:55:28 -0400

    added omniauth google callback route

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :messages
   resources :missions
+
+  get '/auth/:provider/callback' => 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
