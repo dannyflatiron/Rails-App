637e9a500077b0f584a1c62b0758346ca361f4a2
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 03:45:40 -0400

    changed code_name to name

--- db/migrate/20200514061131_devise_create_users.rb
+++ db/migrate/20200514061131_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :code_name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name
       t.integer :uid
 
       ## Recoverable
