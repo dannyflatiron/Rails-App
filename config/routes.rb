0931bdf0ddee2daf8c529655947fe9f18efeb160
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 14:06:40 -0400

    Fixed routes type for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessiions: 'users/sessions'
-  }
   root to: "welcome#home"
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'omniauth' }
+
   
   resources :categories
   resources :messages
   resources :missions
 
-  get '/auth/:provider/callback' => 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
