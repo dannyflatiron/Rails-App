ecbe2ec95a538c68cebb6e7263508e03f3cd5899
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 14:02:27 -0400

    Fixed ENV typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,8 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, 'ENV[GOOGLE_CLIENT_ID]', 'ENV[GOOGLE_SECRET_KEY', scope: 'user,public_repo'
-
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: "userinfo.email,userinfo.profile"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
@@ -282,7 +281,7 @@ Devise.setup do |config|
   #
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
-  # config.omniauth_path_prefix = '/my_engine/users/auth'
+  # config.omniauth_path_prefix = '/users/auth'
 
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
