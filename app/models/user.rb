5d60c0399c3c63e0d03bc22d2006951ba6ea4f13
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 11:35:43 -0400

    permitted name attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are: :recoverable, :rememerable,
   # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
-         :validatable, :omniauthable
+         :validatable, :omniauthable, omniauth_providers: [:google]
 end
