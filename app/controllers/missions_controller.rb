44f7c8611c99c531a30ae42afb5ca836bd2ac0e6
dannyflatiron <dannyflatiron@gmail.com>
2020-05-15 04:37:52 -0400

    Create action and strong params

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,27 @@
 class MissionsController < ApplicationController
+
+    def index
+        @missions = Mission.all
+    end
+
+    def new
+        @mission = Mission.new
+    end
+
+    def create
+        @mission = current_user.missions.build(mission_params)
+        # @mission.user = current_user
+        binding.pry
+        if @mission.save 
+            redirect_to missions_path
+        else
+            redirect_to '/'
+        end
+    end
+
+    private
+
+    def mission_params
+        params.require(:mission).permit(:title, :content)
+    end
 end
