d39a04d4138ef929a5b48cd59a191906a8ab89b7
dannyflatiron <dannyflatiron@gmail.com>
2020-05-15 00:43:12 -0400

    show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :uid, :encrypted_password, :provider )
+    end
 end
