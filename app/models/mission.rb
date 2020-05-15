534fea93ddebac9f49ee929c216b999a668701ea
dannyflatiron <dannyflatiron@gmail.com>
2020-05-15 04:38:31 -0400

    commented belongs_to :category

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,6 @@
 class Mission < ApplicationRecord
   belongs_to :user #gives the reader and writer method
-  belongs_to :category
+  # belongs_to :category
   has_many :messages
   has_many :users, through: :messages #gives only the writer method for the plural 
 end
