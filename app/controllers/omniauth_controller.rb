6bce9812d389a8d34f65553c194ccdbdb6f417d8
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 22:10:34 -0400

    added failure action and corrected typo in google_oauth2 action

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,12 +1,17 @@
 class OmniauthController < ApplicationController
 
-    def passthru
-        @user = User.create_from_provider_data(request.env['omni.auth'])
+    def google_oauth2
+        @user = User.create_from_provider_data(request.env['omniauth.auth'])
             if @user.persisted?
                 sign_in_and_redirect @user
             else
                 flash[:error] = 'There was a problem signing you in through Google. Please register or try again later.'
                 redirect_to new_user_registration_url
             end
+    end
+
+    def failure
+        flash[:error] = "There was a problem signing you in. Please register or try signing in later."
+        redirect_to new_user_registration_url
     end
 end
