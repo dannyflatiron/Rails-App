ee0e14061d232c0f2e54fbe8d7e17802e6677fe1
dannyflatiron <dannyflatiron@gmail.com>
2020-05-14 22:45:19 -0400

    Created macro relationships

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,6 @@
 class Mission < ApplicationRecord
-  belongs_to :user
+  belongs_to :user #gives the reader and writer method
   belongs_to :category
   has_many :messages
-  has_many :user, through: :comments
+  has_many :users, through: :messages #gives only the writer method for the plural 
 end
