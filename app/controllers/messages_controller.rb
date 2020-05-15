3e25f6a3b49853166657b761bd519a4a24cc25f9
dannyflatiron <dannyflatiron@gmail.com>
2020-05-15 05:50:53 -0400

    added index show edit new create update actions and strong params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,45 @@
 class MessagesController < ApplicationController
 
 
+    def index 
+        if params[:mission_id] && @mission = Mission.find_by_id(params[:mission_id])
+            @messages = @mission.messages
+        else
+            @error = "That mission does not exist" if params[:mission_id]
+            @messages = Message.all
+        end
+    end
+
+    def new
+        @message = Message.new
+    end
+
+    def create 
+        @message = current_user.messages.build(message_params)
+        if @message.save
+            redirect_to messages_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @message = Message.find_by(id: params[:id])
+    end
+
+    def edit
+        @message = Message.find_by(id: params[:id])
+    end
     
+    def update
+        @message = Message.find_by(id: params[:id])
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:content)
+    end
+
+
 end
