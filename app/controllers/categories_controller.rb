0b6dd35a469be94ff582ebd1e42b09194ab2acf9
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 12:57:20 -0400

    Cleaned up code. Deleted unused actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,27 +8,5 @@ class CategoriesController < ApplicationController
         @category = Category.find_by_id(params[:id])
     end
 
-    # def new
-    #   if params[:user_id] && @user = User.find_by_id(params[:user_id])
-    #     @category= @user.categories.build
-    #   else
-    #     @category= Category.new
-    #   end
-    # end
-
-    # def create
-    #   @category = current_user.categories.build(category_params)
-    #   if @category.save 
-    #     redirect_to categories_path
-    #   else
-    #     render :new
-    #   end
-    # end
-
-    # private
-
-    # def message_params
-    #     params.require(:category).permit(:content)
-    #   end
 
 end
