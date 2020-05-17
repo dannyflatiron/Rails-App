9d71621484243779dfc60c864340a01dfa3a8530
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 04:55:30 -0400

    build categories ontop of instance variable from new action, same logic to the edit action and added category_attributes and category_id to mission strong params

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -11,12 +11,18 @@ class MissionsController < ApplicationController
       end    
     end
 
+    def show
+      @mission = Mission.find_by_id(params[:id])
+      redirect_to missions_path if !@mission
+    end
+
     def new
       if params[:user_id] && @user = User.find_by_id(params[:user_id])
         @mission = @user.missions.build
       else
         @mission = Mission.new
       end
+      @mission.build_category
     end
 
     def create
@@ -28,14 +34,30 @@ class MissionsController < ApplicationController
       end
     end
 
-    def show
-      @mission = Mission.find_by_id(params[:id])
-      redirect_to missions_path if !@mission
+    def edit
+      @mission = Mission.find_by(params[:id])
+      redirect to missions_path if !@mission || @mission.user != current_user
+      @mission.build_category if !@mission.category
     end
+
+    def update
+      @mission = Mission.find_by(params[:id])
+      redirect to missions_path if !@mission || @mission.user != current_user
+      if @mission.update(mission_params)
+        redirect_to mission_path(@mission)
+      end
+    end
+
+    def destroy
+      Mission.find_by(params[:id]).destroy
+      redirect_to user_path(current_user)
+    end
+
+
 
     private
 
     def mission_params
-      params.require(:mission).permit(:title, :content)
+      params.require(:mission).permit(:title, :content, :category_id, category_attributes: [:name])
     end
 end
