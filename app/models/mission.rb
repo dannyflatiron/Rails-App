5e7725aba60f8dad8fbdce7406e5e50afe4e4c7a
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 04:56:55 -0400

    enabled belongs_to category and corrected parameters in category_attribtues

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,6 @@
 class Mission < ApplicationRecord
   belongs_to :user #gives the reader and writer method
-  # belongs_to :category
+  belongs_to :category
   has_many :messages
   has_many :users, through: :messages #gives only the writer method for the plural 
   validates :content, :title, presence: true
@@ -13,7 +13,7 @@ class Mission < ApplicationRecord
 
   # makes more sense to use find or create by instead of first or create
   # since first or create is typically used when paramaters are already present
-  def category_attributes(attr)
+  def category_attributes=(attr)
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
   end
 end
