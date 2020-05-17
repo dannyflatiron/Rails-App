a2cb58207d2b28da11f119a30f3adec23bb867b0
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 15:57:55 -0400

    created send_coded_message action

--- app/mailers/encoded_message_mailer.rb
+++ app/mailers/encoded_message_mailer.rb
@@ -1,2 +1,9 @@
 class EncodedMessageMailer < ApplicationMailer
+
+    def send_coded_message(receiver, sender, message)
+        @receiver = receiver
+        @sender = sender
+        @message = message
+    end
+
 end
