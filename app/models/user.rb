63b7dafd84febccd32cde8ead000854053cff241
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 16:49:19 -0400

    added method decodeMorse

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,93 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
     end
   end
+
+
+  def decodeMorse(string)
+    binding.pry
+    words = string.split("   ")
+    result = words.map do |word|
+      phrase = ""
+      letters = word.split(" ")
+      letters.each do |letter|
+        case letter
+        when ".-"
+          phrase << "a"
+        when "-..."
+          phrase << "b"
+        when "-.-."
+          phrase << "c"
+        when "-.."
+          phrase << "d"
+        when "."
+          phrase << "e"
+        when "..-."
+          phrase << "f"
+        when "--."
+          phrase << "g"
+        when "...."
+          phrase << "h"
+        when ".."
+          phrase << "i"
+        when ".---"
+          phrase << "j"
+        when "-.-"
+          phrase << "k"
+        when ".-.."
+          phrase << "l"
+        when "--"
+          phrase << "m"
+        when "-."
+          phrase << "n"
+        when "---"
+          phrase << "o"
+        when ".--."
+          phrase << "p"
+        when "--.-"
+          phrase << "q"
+        when ".-."
+          phrase << "r"
+        when "..."
+          phrase << "s"
+        when "-"
+          phrase << "t"
+        when "..-"
+          phrase << "u" 
+        when "...-"
+          phrase << "v"
+        when ".--"
+          phrase << "w"
+        when "-..-"
+          phrase << "x"
+        when "-.--"
+          phrase << "y"
+        when "--.."
+          phrase << "z"  
+        when "-----"
+          phrase << "0"
+        when ".----"
+          phrase << "1"
+        when "..---"
+          phrase << "2"
+        when "...--"
+          phrase << "3"
+        when "....-"
+          phrase << "4"
+        when "-----"
+          phrase << "5"
+        when "-...."
+          phrase << "6"
+        when "--..."
+          phrase << "7"
+        when "---.."
+          phrase << "8" 
+        when "----."
+          phrase << "9"
+        end
+      end
+      phrase
+    end
+    result.join(" ")
+  end
+
 end
