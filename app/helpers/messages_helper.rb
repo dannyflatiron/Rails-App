55dda132b55f6988aac53e5528073bc7aa8af1eb
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 11:07:04 -0400

    Helper method does not work

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,24 @@
 module MessagesHelper
+
+    def index_display_header
+    #     if @mission    
+    #     "Messages for Mission: #{@mission.title}"
+    #     @messages.each do |message| 
+    #       "Secret Agent: message.user.name says message.content" 
+    #     end 
+         
+    #    else 
+    #     content_tag(:h1, "All Messages")
+    #     @messages.each do |m|
+    #     "Secret Agent #{m.user.name} says #{m.content} about this mission:  link_to('m.mission.title', mission_path(#{m.mission_id})"
+    #     end 
+       
+      
+ 
+
+    # @mission.title
+    #       end 
+       
+    end
+
 end
