ecb0d5747363629c85a4bdbe9e908f4ce7bb214b
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 04:59:32 -0400

    added resources categories

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   resources :missions do 
     resources :messages
   end
+
+  resources :categories
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
