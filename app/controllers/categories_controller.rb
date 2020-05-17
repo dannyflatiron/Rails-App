2bc0463171a67cb210f0f9e00cf44eefd15b37db
dannyflatiron <dannyflatiron@gmail.com>
2020-05-17 04:52:57 -0400

    created show and index actions. Removed remaining actions and strong_params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,34 @@
 class CategoriesController < ApplicationController
+
+    def index
+        @categories = Category.all.includes(:posts)
+    end
+
+    def show 
+        @category = Category.find_by_id(params[:id])
+    end
+
+    # def new
+    #   if params[:user_id] && @user = User.find_by_id(params[:user_id])
+    #     @category= @user.categories.build
+    #   else
+    #     @category= Category.new
+    #   end
+    # end
+
+    # def create
+    #   @category = current_user.categories.build(category_params)
+    #   if @category.save 
+    #     redirect_to categories_path
+    #   else
+    #     render :new
+    #   end
+    # end
+
+    # private
+
+    # def message_params
+    #     params.require(:category).permit(:content)
+    #   end
+
 end
